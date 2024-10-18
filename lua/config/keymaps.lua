609fd3e21281886fb20482d2187cd8d20e86cc66
huangshuai <huangshuai@ctfo.com>
2024-10-18 16:44:18 +0800

    editor: Pressing <esc>j or <esc>k quickly will tringer this keymap, I don't like it

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Pressing <esc>j or <esc>k quickly will tringer this keymap, I don't like it
+vim.keymap.del({ "i", "n", "v" }, "<A-k>")
+vim.keymap.del({ "i", "n", "v" }, "<A-j>")
