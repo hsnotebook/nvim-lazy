9f8a74b63e44c693478e3559efee03701b47151e
huangshuai <huangshuai@ctfo.com>
2025-03-11 12:18:33 +0800

    sql: set keymap in sql file

--- lua/plugins/sql.lua
+++ lua/plugins/sql.lua
@@ -3,6 +3,7 @@ return {
     "kristijanhusak/vim-dadbod-ui",
     keys = {
       { "<cr>", "vap|<Plug>(DBUI_ExecuteQuery)", ft = "mysql" },
+      { "<cr>", "<Plug>(DBUI_ExecuteQuery)", mode = "v", ft = "mysql" },
     },
   },
 }
