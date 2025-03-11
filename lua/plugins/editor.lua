df4869ba2d8528f418201e99bd6a40cdac402a02
huangshuai <huangshuai@ctfo.com>
2025-03-11 12:16:03 +0800

    editor: add taboo

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,64 +1,65 @@
 return {
-  {
-    "lilydjwg/fcitx.vim",
-    config = function()
-      vim.opt.ttimeoutlen = 100
-    end,
-  },
-  {
-    "ibhagwan/fzf-lua",
-    opts = {
-      actions = {
-        files = {
-          true,
-          ["ctrl-x"] = require("fzf-lua").actions.file_split,
-        },
-      },
-      files = {
-        fd_opts = [[--color=never --type f --hidden --follow -E .git -E .obsidian]],
-      },
-    },
-    keys = {
-      { "<leader>gc", false },
-      { "<leader>gs", false },
-    },
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      window = {
-        mappings = {
-          ["x"] = "open_split",
-          ["v"] = "open_vsplit",
-        },
-      },
-      filesystem = {
-        follow_current_file = { enabled = false },
-        group_empty_dirs = true,
-      },
-    },
-    keys = {
-      { "<leader>fs", "<cmd>Neotree reveal_force_cwd<cr>", desc = "Show in Neotree" },
-    },
-  },
+	{
+		"lilydjwg/fcitx.vim",
+		config = function()
+			vim.opt.ttimeoutlen = 100
+		end,
+	},
+	{
+		"ibhagwan/fzf-lua",
+		opts = {
+			actions = {
+				files = {
+					true,
+					["ctrl-x"] = require("fzf-lua").actions.file_split,
+				},
+			},
+			files = {
+				fd_opts = [[--color=never --type f --hidden --follow -E .git -E .obsidian]],
+			},
+		},
+		keys = {
+			{ "<leader>gc", false },
+			{ "<leader>gs", false },
+		},
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			window = {
+				mappings = {
+					["x"] = "open_split",
+					["v"] = "open_vsplit",
+				},
+			},
+			filesystem = {
+				follow_current_file = { enabled = false },
+				group_empty_dirs = true,
+			},
+		},
+		keys = {
+			{ "<leader>fs", "<cmd>Neotree reveal_force_cwd<cr>", desc = "Show in Neotree" },
+		},
+	},
 
-  {
-    "hedyhli/outline.nvim",
-    optional = true,
-    opts = {
-      keymaps = {
-        up_and_jump = "<c-k>",
-        down_and_jump = "<c-j>",
-      },
-    },
-  },
-  { "lambdalisue/vim-suda" },
-  { "mg979/vim-visual-multi" },
-  { "tpope/vim-abolish" },
-  {
-    "jpalardy/vim-slime",
-    init = function()
-      vim.g.slime_target = "tmux"
-    end,
-  },
+	{
+		"hedyhli/outline.nvim",
+		optional = true,
+		opts = {
+			keymaps = {
+				up_and_jump = "<c-k>",
+				down_and_jump = "<c-j>",
+			},
+		},
+	},
+	{ "lambdalisue/vim-suda" },
+	{ "mg979/vim-visual-multi" },
+	{ "tpope/vim-abolish" },
+	{
+		"jpalardy/vim-slime",
+		init = function()
+			vim.g.slime_target = "tmux"
+		end,
+	},
+	{ "gcmt/taboo.vim" },
 }
