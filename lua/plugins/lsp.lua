dbc3aa776a13a9eb35a8ff7c3382534017987aa5
huangshuai <huangshuai@ctfo.com>
2025-04-27 16:31:56 +0800

    lsp: update lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,12 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = { inlay_hints = { enabled = false } },
+    opts = {
+      inlay_hints = { enabled = false },
+      diagnostics = {
+        underline = false,
+        virtual_text = { current_line = true },
+      },
+    },
   },
 }
