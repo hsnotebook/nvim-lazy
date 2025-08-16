310108092af8bd8dae11079bd1f443109a83cee6
huangshuai <huangshuai@ctfo.com>
2025-08-16 13:23:23 +0800

    sql: update

--- lua/plugins/sql.lua
+++ lua/plugins/sql.lua
@@ -1,9 +1,21 @@
 return {
-  {
-    "kristijanhusak/vim-dadbod-ui",
-    keys = {
-      { "<cr>", "vap|<Plug>(DBUI_ExecuteQuery)", ft = "mysql" },
-      { "<cr>", "<Plug>(DBUI_ExecuteQuery)", mode = "v", ft = "mysql" },
-    },
+  "kristijanhusak/vim-dadbod-ui",
+  dependencies = {
+    { "tpope/vim-dadbod", lazy = true },
+    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
   },
+  keys = {
+    { "<cr>", "vap|<Plug>(DBUI_ExecuteQuery)", ft = "mysql" },
+    { "<cr>", "<Plug>(DBUI_ExecuteQuery)", mode = "v", ft = "mysql" },
+  },
+  cmd = {
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
 }
