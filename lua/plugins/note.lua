f85f8ba328d3c3f22df620bd9451afe702bed6a8
huangshuai <huangshuai@ctfo.com>
2025-11-01 21:29:40 +0800

    note: The shortcut key <leader>ni behaves differently in the job and life workspaces
    In job workspaces, open Monday diary in the week, and in life workspace
    open current day diary.

--- lua/plugins/note.lua
+++ lua/plugins/note.lua
@@ -1,3 +1,44 @@
+local myWorkspaces = {
+  { name = "hbjg", path = "~/wiki/jobs/hbjg", tag = "job" },
+  { name = "personal", path = "~/wiki/personal", tag = "life" },
+  { name = "ctfo", path = "~/wiki/jobs/ctfo", tag = "job" },
+}
+
+local function get_current_workspace_info()
+  local current_workspace = Obsidian.workspace
+
+  for _, ws in pairs(myWorkspaces) do
+    -- 将配置中的路径展开为绝对路径，并进行比较
+    if ws.name == current_workspace.name then
+      return ws
+    end
+  end
+  return nil -- 没有找到匹配的工作空间
+end
+
+local function OpenMondayNote()
+  local ws_info = get_current_workspace_info()
+  if ws_info and ws_info.tag == "job" then
+    -- 获取当前日期和时间
+    local now = os.date("*t")
+    local current_wday = now.wday
+
+    -- 计算到本周一的偏移天数
+    -- 注意: os.date 中 wday 为 1(周日) 到 7(周六)
+    local offsetDays
+    if current_wday == 1 then
+      offsetDays = -6
+    else
+      offsetDays = -(current_wday - 2) -- 2 对应周一
+    end
+    local command = "silent ObsidianToday " .. offsetDays
+    vim.cmd(command)
+  -- 执行打开周一日记的逻辑
+  else
+    vim.cmd("silent ObsidianToday")
+  end
+end
+
 return {
   {
     "folke/which-key.nvim",
@@ -15,20 +56,7 @@ return {
     ft = "markdown",
     lazy = false,
     opts = {
-      workspaces = {
-        {
-          name = "hbjg",
-          path = "~/wiki/jobs/hbjg",
-        },
-        {
-          name = "personal",
-          path = "~/wiki/personal",
-        },
-        {
-          name = "ctfo",
-          path = "~/wiki/jobs/ctfo",
-        },
-      },
+      workspaces = myWorkspaces,
       notes_subdir = "0.inbox",
       daily_notes = {
         folder = "diary",
@@ -72,7 +100,8 @@ return {
     },
     keys = {
       { "<leader>nw", "<cmd>Obsidian workspace<cr>", desc = "Change Workspace" },
-      { "<leader>ni", "<cmd>Obsidian today<cr>", desc = "Open Today Note" },
+      -- { "<leader>ni", "<cmd>Obsidian today<cr>", desc = "Open Today Note" },
+      { "<leader>ni", OpenMondayNote, desc = "Open Today Note" },
       { "<leader>nf", "<cmd>Obsidian quick_switch<cr>", desc = "Find Notes" },
       { "<leader>n/", "<cmd>Obsidian search<cr>", desc = "Search Notes" },
       { "<leader>no", "<cmd>Obsidian open<cr>", desc = "Open in Obsidian" },
